16400d9d2ac7f9b3ddc9c3fc9dc08c93f5395498
OmTejaswi <75362653+OmTejaswi@users.noreply.github.com>
2021-06-27 12:02:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
+
+document.addEventListener("keydown", (event) =>{
+    if(event.keyCode === 91) {
+        if(startmenu.style.bottom == "50px"){
+            startmenu.style.bottom = "-655px"
+        }
+        else{
+            startmenu.style.bottom = "50px"
+        }
+    }
+})
